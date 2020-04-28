3ddaf2ce378969a53eee4e70f0593abe6881e3a2
aavinash9 <42169836+aavinash9@users.noreply.github.com>
2020-04-28 19:11:27 +0530

    Added files for the exploratory data analysis assignment 1

---
+++ plot1.R
@@ -0,0 +1,10 @@
+plotData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
+subplotdata <- subset(plotData, Date %in% c("1/2/2007","2/2/2007"))
+
+png("plot1.png", width = 480, height = 480)
+
+hist(as.numeric(subplotdata$Global_active_power), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+dev.off()
+
---
+++ plot3.R
@@ -0,0 +1,18 @@
+plotData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
+subplotdata <- subset(plotData, Date %in% c("1/2/2007","2/2/2007"))
+
+datetime <- strptime(paste(subplotdata$Date, subplotdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+png("plot3.png", width = 480, height = 480)
+
+plot(datetime, as.numeric(subplotdata$Sub_metering_1), type="l", ylab="Energy Submetering", xlab="")
+
+lines(datetime, as.numeric(subplotdata$Sub_metering_2), type="l", col="red")
+
+lines(datetime, as.numeric(subplotdata$Sub_metering_3), type="l", col="blue")
+
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+dev.off()
+
